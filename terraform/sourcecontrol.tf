8876fbe5839b68385f6ed7a8e69f3390e7235189
Prashant-Kumar-104 <72803281+Prashant-Kumar-104@users.noreply.github.com>
2023-08-10 22:18:25 +0530

    token-update

--- terraform/sourcecontrol.tf
+++ terraform/sourcecontrol.tf
@@ -1,10 +1,10 @@
 resource "azurerm_app_service_source_control" "src2" {
   app_id   = azurerm_linux_web_app.serviceapp.id
-  repo_url = "https://github.com/Prashant-Kumar-104/Azure.git"
-  branch   = "master"
+  repo_url = "https://github.com/Prashant-Kumar-104/ASPNET-Core-6-with-Vite-and-Vue-3-Base-Template.git"
+  branch   = "main"
 }
 
 resource "azurerm_source_control_token" "token12" {
   type  = "GitHub"
-  token = "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"
+  token = "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"
 }
